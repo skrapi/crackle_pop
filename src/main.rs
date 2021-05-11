5677c24012c510bc51fe68692c4147d27d9451a2
Sylvan <sylvan.j.morris@gmail.com>
2021-05-11 17:56:14 +0200

    Created solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn find_solution(n: usize) -> String {
+    if n % 15 == 0 {
+        "CracklePop".to_string()
+    } else if n % 5 == 0 {
+        "Pop".to_string()
+    } else if n % 3 == 0 {
+        "Crackle".to_string()
+    } else {
+        n.to_string()
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    for n in 1..101 {
+        println!("{}", find_solution(n));
+    }
 }
